a6c8583604071843f673d1b274c26298a44e22c5
jwhaynes <jhaynes2112@gmail.com>
2023-01-16 19:42:19 -0500

    added solutions to game class

--- tic_tac_toe_classes.py
+++ tic_tac_toe_classes.py
@@ -4,4 +4,17 @@ class Player():
     pass
 
 class Game():
-    pass
+    
+    solutions = [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9],
+        [1,4,7],
+        [2,5,8],
+        [3,6,9],
+        [1,5,9],
+        [3,5,7]
+    ]
+
+    def check_for_win(self):
+        pass
